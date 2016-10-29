4974057547f04140c02a5cea99c8fe1598d7be66
Nir Arazi <nir.arazi@gmail.com>
2016-10-29 11:11:57 +0300

    Changed to using document ID's hex string instead of the object itself

--- aggregate.js
+++ aggregate.js
@@ -1,56 +1,61 @@
 ReactiveAggregate = function (sub, collection, pipeline, options) {
-  var defaultOptions = {
-    observeSelector: {},
-    observeOptions: {},
-    clientCollection: collection._name
-  };
-  options = _.extend(defaultOptions, options);
+    var defaultOptions = {
+        observeSelector: {},
+        observeOptions: {},
+        clientCollection: collection._name
+    };
+    options = _.extend(defaultOptions, options);
 
-  var initializing = true;
-  sub._ids = {};
-  sub._iteration = 1;
+    var initializing = true;
+    sub._ids = {};
+    sub._iteration = 1;
 
-  function update() {
-    if (initializing) return;
-    // add and update documents on the client
-    collection.aggregate(pipeline).forEach(function (doc) {
-      if (!sub._ids[doc._id]) {
-        sub.added(options.clientCollection, doc._id, doc);
-      } else {
-        sub.changed(options.clientCollection, doc._id, doc);
-      }
-      sub._ids[doc._id] = sub._iteration;
-    });
-    // remove documents not in the result anymore
-    _.forEach(sub._ids, function (v, k) {
-      if (v != sub._iteration) {
-        delete sub._ids[k];
-        sub.removed(options.clientCollection, k);
-      }
-    });
-    sub._iteration++;
-  }
+    function update() {
+        if (initializing) return;
+        // add and update documents on the client
+        collection.aggregate(pipeline).forEach(function (doc) {
+            var id = doc._id;
 
-  // track any changes on the collection used for the aggregation
-  var query = collection.find(options.observeSelector, options.observeOptions);
-  var handle = query.observeChanges({
-    added: update,
-    changed: update,
-    removed: update,
-    error: function (err) {
-      throw err;
+            if (id && id.toHexString) id = id.toHexString();
+
+            if (!sub._ids[id]) {
+                sub.added(options.clientCollection, id, doc);
+            } else {
+                sub.changed(options.clientCollection, id, doc);
+            }
+
+            sub._ids[id] = sub._iteration;
+        });
+        // remove documents not in the result anymore
+        _.forEach(sub._ids, function (v, k) {
+            if (v != sub._iteration) {
+                delete sub._ids[k];
+                sub.removed(options.clientCollection, k);
+            }
+        });
+        sub._iteration++;
     }
-  });
-  // observeChanges() will immediately fire an "added" event for each document in the query
-  // these are skipped using the initializing flag
-  initializing = false;
-  // send an initial result set to the client
-  update();
-  // mark the subscription as ready
-  sub.ready();
 
-  // stop observing the cursor when the client unsubscribes
-  sub.onStop(function () {
-    handle.stop();
-  });
+    // track any changes on the collection used for the aggregation
+    var query = collection.find(options.observeSelector, options.observeOptions);
+    var handle = query.observeChanges({
+        added: update,
+        changed: update,
+        removed: update,
+        error: function (err) {
+            throw err;
+        }
+    });
+    // observeChanges() will immediately fire an "added" event for each document in the query
+    // these are skipped using the initializing flag
+    initializing = false;
+    // send an initial result set to the client
+    update();
+    // mark the subscription as ready
+    sub.ready();
+
+    // stop observing the cursor when the client unsubscribes
+    sub.onStop(function () {
+        handle.stop();
+    });
 };
